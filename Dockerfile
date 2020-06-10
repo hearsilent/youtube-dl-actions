43279ef44720829f85d4bb09b5166662e1b8630c
HearSilent <hear.silent1995@gmail.com>
2020-06-10 12:42:18 +0800

    Init code

---
+++ Dockerfile
@@ -0,0 +1,14 @@
+FROM python:3.8-alpine
+LABEL MAINTAINER="hear.silent1995@gmail.com"
+
+RUN apk add --no-cache tzdata
+RUN apk --update --no-cache add curl
+RUN apk add --no-cache ca-certificates
+
+RUN curl -L https://yt-dl.org/downloads/latest/youtube-dl -o /usr/local/bin/youtube-dl
+RUN chmod a+rx /usr/local/bin/youtube-dl
+
+WORKDIR /
+
+COPY entrypoint.sh /entrypoint.sh
+ENTRYPOINT ["/entrypoint.sh"]
---
+++ entrypoint.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+UPLOAD_TO = "Youtube"
+UPLOAD_FROM = `youtube-dl -i --get-filename -o '%(title)s.%(ext)s' https://www.youtube.com/watch?v=T4SimnaiktU`
+
+curl -X POST \
+    -H "Authorization: Bearer $GOOGLE_DRIVE_ACCESS_TOKEN" \
+    -F "metadata={name : '$UPLOAD_FROM', parents: ['$UPLOAD_TO']};type=application/json;charset=UTF-8" \
+    -F "file=@$UPLOAD_FROM;type=video/mp4" \
+    "https://www.googleapis.com/upload/drive/v3/files?uploadType=multipart"
