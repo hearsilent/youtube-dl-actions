b73c270ee474ab624044661972ce75a33ffc4348
HearSilent <hear.silent1995@gmail.com>
2020-06-10 13:25:55 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
 
-UPLOAD_TO="YouTube"
-UPLOAD_FROM=`youtube-dl -i --get-filename -o '/%(title)s.%(ext)s' https://www.youtube.com/watch?v=T4SimnaiktU`
+YOTUBE_URL="https://www.youtube.com/watch?v=T4SimnaiktU"
+UPLOAD_FROM=`youtube-dl -i --get-filename -o '%(title)s.%(ext)s' $YOTUBE_URL`
+youtube-dl -i -o '%(title)s.%(ext)s' $YOTUBE_URL
 echo $UPLOAD_FROM
 
 curl -X POST \
     -H "Authorization: Bearer $GOOGLE_DRIVE_ACCESS_TOKEN" \
-    -F "metadata={name : '$UPLOAD_FROM', parents: ['$UPLOAD_TO']};type=application/json;charset=UTF-8" \
+    -F "metadata={name : '$UPLOAD_FROM', parents: ['$GOOGLE_DRIVE_FOLDER_ID']};type=application/json;charset=UTF-8" \
     -F "file=@\"$UPLOAD_FROM\";type=video/mp4" \
     "https://www.googleapis.com/upload/drive/v3/files?uploadType=multipart"
